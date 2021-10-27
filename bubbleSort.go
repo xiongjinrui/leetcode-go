79e0476c33ad10a67d2f327a233f1d261b768a81
Xiong Jinrui <xiongjinrui@gmail.com>
2021-10-27 17:29:53 +0800

    init

---
+++ bubbleSort.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func bubbleSort(arr []int) {
+	if arr == nil || len(arr) < 2 {
+		return
+	}
+
+	// 除去沉底的最大数，继续遍历
+	for i := len(arr) - 1; i > 0; i-- {
+		fmt.Println("----------------------------")
+		// 将最大数通过一次遍历沉底
+		for j := 0; j < i; j++ {
+			if arr[j] > arr[j+1] {
+				swap(arr, j, j+1)
+			}
+			fmt.Println(arr)
+		}
+	}
+}
+
+// 通过异或运算来交换。
+// 省空间，且运算速度快。
+// 但i, j不能相等。 同数异或将变为0。
+// a = a ^ b
+// b = (a ^ b) ^ b = a
+// a = (a ^ b) ^ a = b
+func swap(arr []int, i int, j int) {
+	arr[i] = arr[i] ^ arr[j]
+	arr[j] = arr[i] ^ arr[j]
+	arr[i] = arr[i] ^ arr[j]
+}
+
+func main() {
+	var arr1 = []int{5, 10, 9, 35, 5, 1, 8, 1009, 435}
+
+	bubbleSort(arr1)
+}
---
+++ printOddTimesNum.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+func printOneOddTimesNum(arr []int) {
+	eor := 0
+
+	// {1, 2, 3, 1, 1, 3, 2}
+	// (a^b)^c = a^(b^c) 异或交换律、
+	// 偶数个异或完为0，奇数个异或完只剩自己，一起就只剩奇数个自己
+	// 1 ^ 1 = 0, 0 ^ 0 = 0, 0 ^ 1 = 1
+	for _, v := range arr {
+		eor ^= v
+	}
+
+	fmt.Println(eor)
+}
+
+func printTwoOddTimesNum(arr []int) {
+	eor := 0
+
+	// eor = a ^ b 且 != 0
+	for _, v := range arr {
+		eor ^= v
+	}
+
+	// eor必有最右一位为1的位置，因为eor = a^b且a,b不相等。
+	// 通过这个位置上的值为0，1来将数组分组，且a, b必分在不同组。
+	// 此位置按如下方法选取最右为1的位置。
+	// 1001 0100
+	// 0110 1011 + 1 = 0110 1100
+	// 1001 0100 & 0110 1100 = 0000 0100
+	rightOne := eor & (^eor + 1)
+
+	eor1 := 0
+	for _, v := range arr {
+		// 只选取最右为1位置上所有为1的数，a或b之一必在其中
+		// eor1最终必为a或b之一
+		// 另一个数就是eor ^ eor1
+		// == 0 或者 == rightOne，不能 == 1。 rightOne = 0000 0100
+		if v&rightOne == 0 {
+			eor1 ^= v
+		}
+	}
+
+	fmt.Printf("%d  %d", eor1, eor^eor1)
+}
+
+func main() {
+	arr1 := []int{1, 2, 5, 7, 5, 5, 7, 2, 1, 1, 1}
+	arr2 := []int{1, 2, 5, 1, 7, 5, 5, 7, 2, 1, 1, 1}
+
+	// 找出数组中唯一出现奇数次数的数
+	printOneOddTimesNum(arr1)
+
+	// 找出数组中两个出现奇数次数的数
+	printTwoOddTimesNum(arr2)
+}
